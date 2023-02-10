72822fbd0fcfb4f3a94772049aa6f458fcc2e263
Nazmul Hasan Nannu <nazmulnannu43@gmail.com>
2023-02-11 01:41:12 +0600

    working on 160

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Roboto_Slab } from "@next/font/google";
+
+const roboto = Roboto_Slab({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
